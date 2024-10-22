b086997f5e1dca25c53d9ec6f0bce80f247e42f3
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-10-22 18:09:23 -0400

    added the fish container

--- lib/main.dart
+++ lib/main.dart
@@ -24,14 +24,70 @@ class AquariumScreen extends StatefulWidget {
 }
 
 class _AquariumScreenState extends State<AquariumScreen> {
+  List<Fish> fishList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Virtual Aquarium'),
       ),
-      body: Center(
-        child: Text('Aquarium!'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: 300,
+              height: 300,
+              color: Colors.lightBlueAccent,
+              child: Stack(
+                children: fishList.map((fish) => AnimatedFish(fish: fish)).toList(),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: addFish, 
+                child: Text('Add Fish'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void addFish(){
+    if(fishList.length <10){
+      setState(() {
+        fishList.add(Fish(color: Colors.blue, speed: 1.0));
+      });
+    }
+  }
+}
+
+
+class Fish{
+  final Color color;
+  final double speed;
+  Fish({required this.color, required this.speed});
+}
+
+class AnimatedFish extends StatelessWidget{
+  final Fish fish;
+
+  AnimatedFish({required this.fish});
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: 50,
+      top: 50,
+      child: Container(
+        width: 20,
+        height: 20,
+        decoration: BoxDecoration(
+          color: fish.color,
+          shape: BoxShape.circle,
+        ),
       ),
     );
   }
