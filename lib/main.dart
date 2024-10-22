94135f7add908d5ad81ed9f161fe4f3a2800587c
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-10-22 18:37:09 -0400

    added the fishes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -73,22 +74,94 @@ class Fish{
   Fish({required this.color, required this.speed});
 }
 
-class AnimatedFish extends StatelessWidget{
+class AnimatedFish extends StatefulWidget {
   final Fish fish;
 
   AnimatedFish({required this.fish});
+
+  @override
+  _AnimatedFishState createState() => _AnimatedFishState();
+}
+
+class _AnimatedFishState extends State<AnimatedFish> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _position;
+
+  final double aquariumWidth = 300; 
+  final double aquariumHeight = 300;
+
+  Offset currentPosition = Offset(0, 0);
+  Offset destination = Offset(0, 0);
+
+  @override
+  void initState() {
+    super.initState();
+
+    currentPosition = Offset(
+      Random().nextDouble() * aquariumWidth,
+      Random().nextDouble() * aquariumHeight,
+    );
+
+    destination = _getRandomPosition();
+
+    _controller = AnimationController(
+      duration: Duration(seconds: (5 / widget.fish.speed).round()), 
+      vsync: this,
+    )..forward();
+
+    _position = Tween<Offset>(begin: currentPosition, end: destination)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.linear))
+      ..addListener(() {
+        setState(() {});  
+      });
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _setNewDestination();
+      }
+    });
+  }
+
+  
+  Offset _getRandomPosition() {
+    return Offset(
+      Random().nextDouble() * (aquariumWidth - 20),
+      Random().nextDouble() * (aquariumHeight - 20), 
+    );
+  }
+
+  
+  void _setNewDestination() {
+    setState(() {
+      currentPosition = destination;
+      destination = _getRandomPosition();  
+
+      _position = Tween<Offset>(begin: currentPosition, end: destination)
+          .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+
+      _controller.forward(from: 0);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: 50,
-      top: 50,
+      left: _position.value.dx,
+      top: _position.value.dy,
       child: Container(
         width: 20,
         height: 20,
         decoration: BoxDecoration(
-          color: fish.color,
+          color: widget.fish.color,
           shape: BoxShape.circle,
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
